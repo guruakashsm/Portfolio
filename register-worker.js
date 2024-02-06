8c59f5e103f9e8866425516985370d8fbf9868c4
GURUAKASHSM <109951850+GURUAKASHSM@users.noreply.github.com>
2024-02-06 15:16:52 +0530

    Delete register-worker.js

--- register-worker.js
+++
@@ -1,9 +0,0 @@
-/**
- * Register the service worker
- */
-console.log('In');
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker
-		.register('./service-worker.js')
-		.then(function () { console.log('Service Worker Registered'); });
-}
